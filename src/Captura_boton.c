75684b601e735c40e228d7ae4402caab6801c5e1
MatiasLopezViagel <100703037+MatiasLopezViagel@users.noreply.github.com>
2026-01-18 15:07:12 +0100

    Add files via upload

---
+++ src/Captura_boton.c
@@ -0,0 +1,95 @@
+/*
+ * Captura_boton.c
+ *
+ *  Created on: Dec 31, 2025
+ *      Author: LENOVO
+ */
+
+
+#include "Captura_Boton.h"
+#include <stdlib.h>
+
+/* Handles externos */
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim4;
+extern ADC_HandleTypeDef hadc1;
+
+/* Variables externas (tal como las usabas) */
+extern volatile uint32_t tiempo_ms;
+extern volatile uint8_t boton_pulsado;
+extern volatile uint32_t resultado_final;
+extern volatile uint16_t adc_ruido;
+extern uint8_t modo_actual;
+
+/* ===== INIT ===== */
+void Captura_Boton_Init(void)
+{
+    HAL_ADC_Start_IT(&hadc1);
+    HAL_Delay(100);
+    srand(adc_ruido);
+}
+
+/* ===== RUN ===== */
+void Captura_Boton_Run(void)
+{
+    /* 1. Limpieza inicial */
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1 | GPIO_PIN_2, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+
+    boton_pulsado = 0;
+    resultado_final = 0;
+
+    /* 2. Obtener Azar */
+    uint32_t tiempo_espera_azar = (rand() % 3001) + 1000;
+
+    /* 3. Espera */
+    uint32_t tiempo_inicio_espera = HAL_GetTick();
+
+    /* 4. Inicio ronda */
+    tiempo_ms = 0;
+
+    if (modo_actual == MODO_INDIVIDUAL)
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+    else
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1 | GPIO_PIN_2, GPIO_PIN_SET);
+
+    __HAL_TIM_SET_COUNTER(&htim2, 0);
+    HAL_TIM_Base_Start_IT(&htim2);
+
+    /* 5. Espera pulsación */
+    if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == GPIO_PIN_SET)
+    {
+        while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == GPIO_PIN_SET);
+        HAL_Delay(100);
+
+        while ((HAL_GetTick() - tiempo_inicio_espera) < tiempo_espera_azar)
+        {
+            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+
+            if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == GPIO_PIN_SET)
+            {
+                boton_pulsado = 1;
+                resultado_final = tiempo_ms;
+                HAL_TIM_Base_Stop_IT(&htim2);
+
+                HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1 | GPIO_PIN_2, GPIO_PIN_RESET);
+                HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+
+                __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 32767);
+                HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+            }
+        }
+    }
+
+    resultado_final = tiempo_ms;
+    HAL_TIM_Base_Stop_IT(&htim2);
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1 | GPIO_PIN_2, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+
+    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 32767);
+    HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+
+    if (resultado_final == 0)
+        resultado_final = tiempo_ms;
+}
---
+++ src/Maquina_estados.c
@@ -0,0 +1,110 @@
+/*
+ * Maquina_estados.c
+ *
+ *  Created on: Dec 31, 2025
+ *      Author: LENOVO
+ */
+
+#include "Maquina_estados.h"
+#include <stdlib.h>
+
+/* ====== Handles externos ====== */
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim4;
+
+/* ====== Variables privadas ====== */
+static EstadoJuego_t estado_juego;
+
+static uint8_t ronda_actual;
+static uint16_t puntos_j1, puntos_j2;
+static uint8_t ganador;
+static uint8_t boton_pulsado;
+static uint32_t tiempo_ms;
+static uint32_t resultado_final;
+
+static uint8_t dificultad;
+static uint8_t num_jugadores;
+static ModoJuego_t modo;
+
+static uint32_t tick_inicio;
+static uint32_t tiempo_espera;
+
+/* ====== Funciones privadas ====== */
+static uint16_t calcular_puntos(uint32_t tiempo)
+{
+    if (tiempo < 200) return 100;
+    if (tiempo < 400) return 75;
+    if (tiempo < 600) return 50;
+    if (tiempo < 1000) return 25;
+    return 10;
+}
+
+/* ====== API ====== */
+void Juego_Init(void)
+{
+    estado_juego = ST_JUEGO_INICIO;
+    ronda_actual = 1;
+    puntos_j1 = puntos_j2 = 0;
+}
+
+void Juego_Run(void)
+{
+    switch (estado_juego)
+    {
+        case ST_JUEGO_INICIO:
+            ronda_actual = 1;
+            puntos_j1 = puntos_j2 = 0;
+            estado_juego = ST_JUEGO_TIEMPO_ESPERA;
+            break;
+
+        case ST_JUEGO_TIEMPO_ESPERA:
+            tiempo_espera = (rand() % (2000 / dificultad)) + 1000;
+            tick_inicio = HAL_GetTick();
+            estado_juego = ST_JUEGO_RONDA;
+            break;
+
+        case ST_JUEGO_RONDA:
+            /* Espera no bloqueante */
+            if (!boton_pulsado)
+                return;
+
+            switch (modo)
+            {
+                case MODO_2_SOLO_PUNTOS:
+                    puntos_j1 += calcular_puntos(resultado_final);
+                    break;
+                case MODO_3_DUO_PUNTOS:
+                    if (ganador == 1) puntos_j1 += calcular_puntos(resultado_final);
+                    else if (ganador == 2) puntos_j2 += calcular_puntos(resultado_final);
+                    break;
+                default:
+                    break;
+            }
+
+            boton_pulsado = 0;
+
+            if (ronda_actual++ < 3)
+                estado_juego = ST_JUEGO_TIEMPO_ESPERA;
+            else
+                estado_juego = ST_JUEGO_FIN;
+            break;
+
+        case ST_JUEGO_FIN:
+            HAL_Delay(3000);
+            estado_juego = ST_JUEGO_INICIO;
+            break;
+    }
+}
+
+void Juego_BotonPulsado(uint8_t jugador, uint32_t tiempo)
+{
+    ganador = jugador;
+    resultado_final = tiempo;
+    boton_pulsado = 1;
+}
+
+void Juego_SetModo(ModoJuego_t m)        { modo = m; }
+void Juego_SetJugadores(uint8_t n)       { num_jugadores = n; }
+void Juego_SetDificultad(uint8_t d)      { dificultad = d; }
+
+EstadoJuego_t Juego_GetEstado(void)      { return estado_juego; }
