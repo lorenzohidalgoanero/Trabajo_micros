5c2272b080ba7c3c8066ba3416416765e362be3b
MatiasLopezViagel <100703037+MatiasLopezViagel@users.noreply.github.com>
2026-01-19 20:58:38 +0100

    Update Calculo_puntos.c

--- src/Calculo_puntos.c
+++ src/Calculo_puntos.c
@@ -1,39 +1,53 @@
+/*
+ * Cálculo_puntos.c
+ *
+ *  Created on: Dec 31, 2025
+ *      Author: LENOVO
+ */
+
+
+#include "stm32f4xx_hal.h"
 #include "Calculo_puntos.h"
+#include <stdint.h>
+
 
 /* Variables */
-uint32_t tiempo_jugador_1 = 0;
-uint32_t tiempo_jugador_2 = 0;
-uint32_t puntos_jugador_1 = 0;
-uint32_t puntos_jugador_2 = 0;
-static uint8_t ganador;
-volatile uint32_t tiempo_1 = 0;
-volatile uint32_t tiempo_2 = 0;
+uint32_t tiempo_jugador_1;
+uint32_t tiempo_jugador_2;
+uint32_t puntos_jugador_1;
+uint32_t puntos_jugador_2;
+uint8_t ganador;
+uint8_t punto_ganado;
+volatile uint32_t tiempo_1;
+volatile uint32_t tiempo_2;
 
-void Calculo_Puntos(uint32_t tiempo_final, EstadoJuego_t estado_actual)
+
+
+
+void Calculo_Puntos(uint32_t tiempo_final, EstadoJuego_t estado_actual, ModoJuego_t modo_actual)
 {
-	EstadoJuego_t estado = Juego_GetEstado();
-	ModoJuego_t modo = Juego_GetModo();
 
-	switch (modo)
+
+	switch (modo_actual)
 	{
-	case MODO_1_SOLO_TIEMPO:
-	            /* En ronda normal, guardamos el tiempo del jugador activo */
-	            if (HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_14) == GPIO_PIN_SET)
-	            {
-	                tiempo_jugador_1 = tiempo_final;
-	            }
-	            else
-	            {
-	            	tiempo_jugador_2 = tiempo_final;
-	            }
-	            break;
+			case MODO_1_SOLO_TIEMPO:
+	            	/* en ronda normal, guardamos el tiempo del jugador activo */
+	            	if (HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_14) == GPIO_PIN_SET)
+	            	{
+	            		tiempo_jugador_1 = tiempo_final;
+	            	}
+	            	else
+	            	{
+	            		tiempo_jugador_2 = tiempo_final;
+	            	}
+	            	break;
 
 
 	        case MODO_4_DUO_REFLEJOS:
 	        	switch (estado_actual)
 	        	    {
 	        	        case ST_JUEGO_RONDA:
-	        	            /* En ronda normal, guardamos el tiempo del jugador activo */
+	        	            /* en ronda normal, guardamos el tiempo del jugador activo */
 	        	            if (HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_14) == GPIO_PIN_SET)
 	        	            {
 	        	                tiempo_jugador_1 = tiempo_1;
@@ -51,7 +65,7 @@ void Calculo_Puntos(uint32_t tiempo_final, EstadoJuego_t estado_actual)
 	        	                puntos_jugador_1 = puntos_jugador_1 +1;
 	        	            }
 
-	        	            else						 //Si es más rápido el jugador 2 que el 1
+	        	            else						 //si es más rápido el jugador 2 que el 1
 	        	            {
 	        	            	tiempo_jugador_2 = tiempo_final;
 	        	            	puntos_jugador_2 = puntos_jugador_2 + 1;
@@ -65,21 +79,24 @@ void Calculo_Puntos(uint32_t tiempo_final, EstadoJuego_t estado_actual)
 	        	switch (estado_actual)
 	        	{
 	        	case ST_JUEGO_RONDA:
-	        		        /* En ronda normal, guardamos el tiempo del jugador activo */
-	        		        if (HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_14) == GPIO_PIN_SET)
+	        		punto_ganado=0;
+	        		ganador=0;
+	        		        /* en ronda normal, guardamos el tiempo del jugador activo */
+	        		        if (HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_14) == GPIO_PIN_SET && punto_ganado==0)
 	        		        {
 	        		        	puntos_jugador_1 = puntos_jugador_1 + 1;
+	        		        	punto_ganado=1;
 	        		        	break;
 	        		        }
-	        		        else if (HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_15) == GPIO_PIN_SET)
+	        		        if (HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_15) == GPIO_PIN_SET && punto_ganado==0)
 	        		        {
-	        		        	puntos_jugador_1 = puntos_jugador_1 + 1;
+	        		        	puntos_jugador_2 = puntos_jugador_2 + 1;
+	        		        	punto_ganado=1;
 	        		        	break;
 	        		        }
 	        	}
 	        default:
 	            break;
-
 	}
 
 }
