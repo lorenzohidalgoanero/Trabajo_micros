151adfc9c10aee3755446f0ec9dca9d40a5c05d5
MatiasLopezViagel <100703037+MatiasLopezViagel@users.noreply.github.com>
2026-01-20 15:14:28 +0100

    Update main.h

--- Inc/main.h
+++ Inc/main.h
@@ -31,7 +31,8 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdlib.h>
+extern volatile uint32_t tiempo_ms;
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
