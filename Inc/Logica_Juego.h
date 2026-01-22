55d71830822c52586e03b189a55abf15dae7e1de
lorenzohidalgoanero <lorenzo.hidalgo@alumnos.upm.es>
2026-01-22 21:39:27 +0100

    FINAL Logica_Juego.h

--- Inc/Logica_Juego.h
+++ Inc/Logica_Juego.h
@@ -19,12 +19,13 @@ public:
     bool getSolicitudSalidaMenu() const { return _solicitudSalidaMenu; }
     void setSolicitudSalidaMenu(bool valor) { _solicitudSalidaMenu = valor; }
 
+
 private:
     Estilo_Pantalla* _pantalla;
 
-    // el dibujante se encarga de pintar las escenas del juego (pantallas y marcador)
+    // Dibujante se encarga de pintar las escenas del juego (pantallas y marcador).
     DibujarJuego _dibujante;
-    // gestor de puntuación para llevar el registro de rondas y puntos de cada jugador
+    // Gestor de puntuación para llevar el registro de rondas y puntos de cada jugador.
     GestorPuntuacion _puntuacion;
 
     OpcionJugadores _confJugadores;
@@ -38,15 +39,20 @@ private:
     uint32_t _tickInicio;
     uint32_t _tickInicioDisparo;
     uint32_t _tiempoAleatorio;
-    uint32_t _tiempoReaccion;
+    uint32_t _tiempoReaccion_1;
+    uint32_t _tiempoReaccion_2;
+
+    //flags para el modo 2 jugadores
+    bool flag_jugador_1 = 0;
+    bool flag_jugador_2 = 0;
+
 
     void Buzzer_Beep(uint16_t duracion);
 
-    // metodos auxiliares
+    // Métodos auxiliares
     void visualizar();
     void iniciarRonda();
     uint32_t obtenerTiempoEspera();
 };
 
 #endif
-
