45a31d6cacf21057a70ec938c56e21467da7b53f
Guillermo Rosales <81184697+GuillermoRosale5@users.noreply.github.com>
2026-01-22 20:34:17 +0100

    v2.1_SinBotonNiBuzzer

--- Inc/Captura_boton.h
+++ Inc/Captura_boton.h
@@ -1,15 +1,17 @@
-/*
- * Captura_boton.h
- *
- *  Created on: Dec 31, 2025
- *      Author: LENOVO
- */
 
 #ifndef INC_CAPTURA_BOTON_H_
 #define INC_CAPTURA_BOTON_H_
 
+#include <stdint.h>
+
 void Captura_Boton_Init(void);
 void Captura_Boton_Run(void);
+
+// Devuelve el tiempo de reacción medido en milisegundos desde el inicio de la ronda.
+uint32_t Captura_GetTiempoFinal();
+
+// devuelve el jugador ganador, si no se ha registrado ninguna pulsación aun devuelve 0
+uint8_t Captura_GetGanador();
 
 
 #endif /* INC_CAPTURA_BOTON_H_ */
