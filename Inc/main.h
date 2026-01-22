0cf4307d58fa4c7116b7c223f2f251d57fecae2a
lorenzohidalgoanero <lorenzo.hidalgo@alumnos.upm.es>
2026-01-22 21:40:26 +0100

    FINAL main.h

--- Inc/main.h
+++ Inc/main.h
@@ -63,15 +63,33 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define BOTON_AZUL_Pin GPIO_PIN_0
+#define BOTON_AZUL_GPIO_Port GPIOA
+#define BOTON_AZUL_EXTI_IRQn EXTI0_IRQn
+#define BUZZER_Pin GPIO_PIN_14
+#define BUZZER_GPIO_Port GPIOB
+#define LED_ROJO_Pin GPIO_PIN_14
+#define LED_ROJO_GPIO_Port GPIOD
+#define LED_AZUL_Pin GPIO_PIN_15
+#define LED_AZUL_GPIO_Port GPIOD
+#define BTN_J2_Pin GPIO_PIN_4
+#define BTN_J2_GPIO_Port GPIOD
+#define BTN_J2_EXTI_IRQn EXTI4_IRQn
+#define BTON_J1_Pin GPIO_PIN_6
+#define BTON_J1_GPIO_Port GPIOD
+#define BTON_J1_EXTI_IRQn EXTI9_5_IRQn
 
 /* USER CODE BEGIN Private defines */
 
 
-#define LED_AZUL_GPIO_Port  GPIOD					//para la logica del juego
+/*#define LED_AZUL_GPIO_Port  GPIOD					//para la logica del juego
 #define LED_AZUL_Pin        GPIO_PIN_15
 #define LED_ROJO_GPIO_Port  GPIOD
 #define LED_ROJO_Pin        GPIO_PIN_14
-
+#define BOTON_JUGADOR_1_GPIO_Port  GPIOD
+#define BOTON_JUGADOR_1_Pin        GPIO_PIN_6
+#define BOTON_JUGADOR_2_GPIO_Port  GPIOD
+#define BOTON_JUGADOR_2_Pin        GPIO_PIN_4*/
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
